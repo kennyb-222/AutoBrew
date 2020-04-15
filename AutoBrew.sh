07653325f1686f51eda3aee63ad155e1522c8722
Kenny Botelho <63611879+kennyb-222@users.noreply.github.com>
2020-04-14 21:10:29 -0700

    Update AutoBrew.sh

--- AutoBrew.sh
+++ AutoBrew.sh
@@ -14,8 +14,12 @@ TargetUser=$(echo "show State:/Users/ConsoleUser" | \
     scutil | awk '/Name :/ && ! /loginwindow/ { print $3 }')
 
 # Check if parameter passed to use pre-defined user
-if [[ -n $1 ]]; then
-    TargetUser=$1
+if [[ -n $3 ]]; then
+    # Supporting running the script in Jamf with no specialization via Self Service
+    ConsoleUser=$3
+elif [[ -n $1 ]]; then
+    # Fallback case for the command line initiated method
+    ConsoleUser=$3
 fi
 
 # Install Homebrew | strip out all interactive prompts
