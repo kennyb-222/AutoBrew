a9581231cd16829344190f67e9988a97d3323a61
Kenny Botelho <63611879+kennyb-222@users.noreply.github.com>
2020-04-14 20:55:56 -0700

    Improved mod to homebrew file permissions
    Removed the sledgehammer approach to changing permissions. Now parses output from Homebrew install to modify permissions.

--- AutoBrew.sh
+++ AutoBrew.sh
@@ -7,18 +7,15 @@
 export HOME=$(mktemp -d)
 export USER=root
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+BREW_INSTALL_LOG=$(mktemp)
 
 # Get current logged in user
-ConsoleUser=$(echo "show State:/Users/ConsoleUser" | \
+TargetUser=$(echo "show State:/Users/ConsoleUser" | \
     scutil | awk '/Name :/ && ! /loginwindow/ { print $3 }')
 
 # Check if parameter passed to use pre-defined user
-if [[ -n $3 ]]; then
-    # Supporting running the script in Jamf with no specialization via Self Service
-    ConsoleUser=$3
-elif [[ -n $1 ]]; then
-    # Fallback case for the command line initiated method
-    ConsoleUser=$3
+if [[ -n $1 ]]; then
+    TargetUser=$1
 fi
 
 # Install Homebrew | strip out all interactive prompts
@@ -26,24 +23,30 @@ fi
     https://raw.githubusercontent.com/Homebrew/install/master/install.sh | \
     sed "s/abort \"Don't run this as root\!\"/\
     echo \"WARNING: Running as root...\"/" | \
-    sed 's/  wait_for_user/  :/')"
+    sed 's/  wait_for_user/  :/')" 2>&1 | tee ${BREW_INSTALL_LOG}
 
-# Reset /usr/local permissions for current user
-chown -R "${ConsoleUser}":admin /usr/local && \
-    chmod -R g+rwx /usr/local
+# Reset Homebrew permissions for target user
+brew_file_paths=$(sed '1,/==> This script will install:/d;/==> /,$d' \
+    ${BREW_INSTALL_LOG})
+brew_dir_paths=$(sed '1,/==> The following new directories/d;/==> /,$d' \
+    ${BREW_INSTALL_LOG})
+chown -R "${TargetUser}":admin ${brew_file_paths}
+chown -R "${TargetUser}":admin ${brew_dir_paths}
+chgrp admin /usr/local/bin/
+chmod g+w /usr/local/bin
 
 # Unset home/user environment variables
 unset HOME
 unset USER
 
-# Finish up Homebrew install as logged in user
-sudo -u "${ConsoleUser}" bash -c "/usr/local/bin/brew update --force"
+# Finish up Homebrew install as target user
+sudo -u "${TargetUser}" bash -c "/usr/local/bin/brew update --force"
 
 # Run cleanup before checking in with the doctor
-sudo -u "${ConsoleUser}" bash -c "/usr/local/bin/brew cleanup"
+sudo -u "${TargetUser}" bash -c "/usr/local/bin/brew cleanup"
 
 # Check Homebrew install status
-sudo -u "${ConsoleUser}" bash -c "/usr/local/bin/brew doctor"
+sudo -u "${TargetUser}" bash -c "/usr/local/bin/brew doctor"
 
 # Check with the doctor status to see if everything looks good
 if [[ $? -eq 0 ]]; then
